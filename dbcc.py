cf5075af21d3a880f0cc239b45c3aeabad441d6a
Frank Müller <frmimue@gmail.com>
2019-09-10 11:17:31 +0200

    Basic logic implementation

---
+++ dbcc.py
@@ -0,0 +1,37 @@
+import json
+
+class TextAnalyzer:
+
+    @staticmethod
+    def analyze(text):
+
+        result = {
+            "textLength": {
+                "withSpaces": 0,
+                "withoutSpaces": 0
+            },
+            "wordCount": 0,
+            "characterCount": {}
+        }
+
+        activeWord = False
+
+        for c in text:
+
+            result["textLength"]["withSpaces"] += 1
+
+            if not c.isspace():
+                result["textLength"]["withoutSpaces"] += 1
+
+            if not c.isspace() and not activeWord:
+                activeWord = True
+                result["wordCount"] += 1
+
+            if c.isalpha():
+                if c in result["characterCount"]:
+                    result["characterCount"][c] += 1
+                else:
+                    result["characterCount"][c] = 1
+            
+        return json.dumps(result, sort_keys=True, indent=4)
+            
